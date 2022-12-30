5ad05369beb7fda58846b0e757f1867caebab121
stanhu <stanhu@users.noreply.github.com>
2022-12-30 07:11:08 +0000

    Automated change: Fix sanity tests

--- src/core/ext/filters/client_channel/http_proxy.cc
+++ src/core/ext/filters/client_channel/http_proxy.cc
@@ -20,6 +20,8 @@
 
 #include "src/core/ext/filters/client_channel/http_proxy.h"
 
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <memory>
@@ -55,13 +57,15 @@
 namespace grpc_core {
 namespace {
 
-static bool serverInCIDRRange(std::string server_host, absl::string_view no_proxy_entry) {
+bool serverInCIDRRange(std::string server_host,
+                       absl::string_view no_proxy_entry) {
   auto server_address = StringToSockaddr(server_host, 0);
   if (!server_address.ok()) {
     return false;
   }
 
-  std::vector<absl::string_view> cidr = absl::StrSplit(no_proxy_entry, '/', absl::SkipEmpty());
+  std::vector<absl::string_view> cidr =
+      absl::StrSplit(no_proxy_entry, '/', absl::SkipEmpty());
   if (cidr.size() != 2) {
     return false;
   }
@@ -74,7 +78,8 @@ static bool serverInCIDRRange(std::string server_host, absl::string_view no_prox
   uint32_t mask_bits = atoi(std::string(cidr[1]).c_str());
   grpc_sockaddr_mask_bits(&*proxy_address, mask_bits);
 
-  return grpc_sockaddr_match_subnet(&*server_address, &*proxy_address, mask_bits);
+  return grpc_sockaddr_match_subnet(&*server_address, &*proxy_address,
+                                    mask_bits);
 }
 
 ///
@@ -193,7 +198,7 @@ absl::optional<std::string> HttpProxyMapper::MapName(
       std::vector<absl::string_view> no_proxy_hosts =
           absl::StrSplit(*no_proxy_str, ',', absl::SkipEmpty());
       for (const auto& no_proxy_entry : no_proxy_hosts) {
-	auto entry = absl::StripAsciiWhitespace(no_proxy_entry);
+        auto entry = absl::StripAsciiWhitespace(no_proxy_entry);
 
         if (absl::EndsWithIgnoreCase(server_host, entry) ||
             serverInCIDRRange(server_host, entry)) {
